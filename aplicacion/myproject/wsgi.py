a4faf27fd4460ab44d976e8c5a846238c6dc42c2
ArticFish <101341592+ArticFish@users.noreply.github.com>
2024-12-07 23:14:08 -0300

    prueba 8

--- aplicacion/myproject/wsgi.py
+++ aplicacion/myproject/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 import sys
 
-print("Directorio actual:", os.getcwd())  # Esto imprimirá el directorio actual
 sys.path.append('/app')  # Asegúrate de que esta ruta esté correcta
 
 from django.core.wsgi import get_wsgi_application
